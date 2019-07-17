a833ab0afb457522bd7a753217334db842394f41
ddkauffman <27729134+ddkauffman@users.noreply.github.com>
2019-07-17 12:09:41 -0400

    Merge current work-in-progress into master.  (#1)
    * RAVEN-1 add: error handling for jeta call with response message.
    
    * RAVEN-1 ref: fetch into versioned API.
    
    * RAVEN-1 mod: configuration and config structure.
    
    * RAVEN-1 mod+rm: relative path in config, removed fetch.[views|urls].
    
    * RAVEN-1 add: requirements.txt from jSka environment requirements.
    
    * RAVEN-3 add: base sphinx config.
    
    * RAVEN-3 add: basic documentation.

---
+++ raven/core/apiv1_urls.py
@@ -0,0 +1,39 @@
+from django.conf.urls import url, include
+
+from raven.fetch.api.v1 import views as fetch_views
+from raven.core.api.v1 import views as core_views
+
+api_version = {
+    'MAJOR_VERSION': 1,
+    'MINOR_VERSION': 0,
+    'PATCH_VERSION': 0,
+}
+
+urlpatterns = [
+    url(
+        regex='fetch/plot',
+        view=fetch_views.FetchPlotDataAPIView.as_view(),
+        name='fetch_plot'
+    ),
+    url(
+        regex='fetch/stats',
+        view=fetch_views.MnemonicStatisticsView.as_view(),
+        name='fetch_stats'
+    ),
+    url(
+        regex='fetch/date-range',
+        view=fetch_views.FetchMnemonicDateRangeAPIView.as_view(),
+        name='fetch_date_range'
+    ),
+    url(
+        regex='fetch',
+        view=fetch_views.FetchEngineeringTelemetryAPIView.as_view(),
+        name='fetch'
+    ),
+    url(
+        regex='info',
+        view=core_views.InfoAPIView.as_view(),
+        name='info'
+    ),
+
+]

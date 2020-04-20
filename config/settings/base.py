e698c90d9161146bb57dabaec3e2b036755ebd2c
David Kauffman <dkauffman@stsci.edu>
2020-04-20 15:17:09 -0400

    rm: error

--- config/settings/base.py
+++ config/settings/base.py
@@ -61,7 +61,7 @@ MIDDLEWARE = [
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes==15),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
 

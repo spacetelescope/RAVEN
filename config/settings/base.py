76685228fc96ac6afe04b795eee0776df02dd877
David Kauffman <dkauffman@stsci.edu>
2020-04-22 00:20:27 -0400

    tmp: bump token life.

--- config/settings/base.py
+++ config/settings/base.py
@@ -61,8 +61,8 @@ MIDDLEWARE = [
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=7),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=14),
 }
 
 REST_FRAMEWORK = {

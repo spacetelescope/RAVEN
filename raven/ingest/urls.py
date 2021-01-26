5ab61e5b1bd44b95dcf2179bc20d66dc5e13d545
David Kauffman <dkauffman@stsci.edu>
2021-01-26 16:22:00 -0500

    wip: remove ingest control routes.

--- raven/ingest/urls.py
+++ raven/ingest/urls.py
@@ -4,24 +4,24 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(
-        r'execute',
-        views.ExecuteIngestView.as_view(),
-        name='execute'
-        ),
-    path(
-        r'message',
-        views.IngestMessageView.as_view(),
-        name='message'
-    ),
-    path(
-        r'update/schedule',
-        views.UpdateIngestSchedule.as_view(),
-        name='update_schedule'
-    ),
-    path(
-        r'status',
-        views.IngestStatusView.as_view(),
-        name='status'
-    )
+    # path(
+    #     r'execute',
+    #     views.ExecuteIngestView.as_view(),
+    #     name='execute'
+    #     ),
+    # path(
+    #     r'message',
+    #     views.IngestMessageView.as_view(),
+    #     name='message'
+    # ),
+    # path(
+    #     r'update/schedule',
+    #     views.UpdateIngestSchedule.as_view(),
+    #     name='update_schedule'
+    # ),
+    # path(
+    #     r'status',
+    #     views.IngestStatusView.as_view(),
+    #     name='status'
+    # )
 ]

679fe2f0d8a369673b3938fb07b9a8567c14c5a7
David Kauffman <dkauffman@stsci.edu>
2021-02-09 14:13:00 -0500

    wip: restore plot data

--- raven/fetch/api/v1/views.py
+++ raven/fetch/api/v1/views.py
@@ -261,44 +261,49 @@ class FetchPlotDataAPIView(APIView):
 
     def get(self, request, format='json'):
 
-        # fetch_url = request.build_absolute_uri(reverse('apiv1:fetch'))
+        mnemonic = request.GET.get('mnemonic', None)
+        fetch_url = request.build_absolute_uri(reverse('apiv1:fetch'))
 
-        # tomorrow = datetime.datetime.now() + timedelta(days=1)
-        # default_end_ydoy = f"{tomorrow.timetuple().tm_year}:{tomorrow.timetuple().tm_yday}:00:00:00.000"
-        # mnemonic = request.GET.get('mnemonic', None)
-        # start_of_ydoy = request.GET.get('start_of_range')
-        # end_of_ydoy = request.GET.get(
-        #     'end_of_range',
-        #     default_end_ydoy)
+        tomorrow = datetime.datetime.now() + timedelta(days=1)
+        default_end_ydoy = f"{tomorrow.timetuple().tm_year}:{tomorrow.timetuple().tm_yday}:00:00:00.000"
 
-        # try:
-        #     response = requests.get(fetch_url, params={
-        #             'mnemonic': mnemonic,
-        #             'start_of_ydoy': start_of_ydoy,
-        #             'end_of_ydoy': end_of_ydoy
-        #     })
+        start_of_ydoy = request.GET.get('start_of_range')
 
-        #     plot_data = response.json()
-        #     plot_data[0]['type'] = 'scattergl'
-        #     plot_data[0]['mode'] = 'lines+markers'
-        #     plot_data[0]['line'] = {
-        #             'shape': 'hv',
-        #             'color': 'rgb(30, 110, 162)'
-        #         }
-        #     plot_data[0]['showlegend'] = True
+        end_of_ydoy = request.GET.get(
+            'end_of_range',
+            default_end_ydoy
+        )
 
-        # except Exception as err:
-        #     return HttpResponse(
-        #         json.dumps({
-        #                 'message': err.args[0],
-        #                 'source': 'raven.api',
-        #                 'class': 'FetchPlotDataAPIView',
-        #             },
-        #         ),
-        #         status=status.HTTP_500_INTERNAL_SERVER_ERROR,
-        #         content_type='application/json')
+        try:
+            response = requests.get(fetch_url, params={
+                    'mnemonic': mnemonic,
+                    'start_of_ydoy': start_of_ydoy,
+                    'end_of_ydoy': end_of_ydoy
+            })
 
-        plot_data = 42
+            plot_data = response.json()
+            plot_data[0]['type'] = 'scattergl'
+            plot_data[0]['mode'] = 'lines+markers'
+
+            plot_data[0]['line'] = {
+                    'shape': 'hv',
+                    'color': 'rgb(30, 110, 162)'
+            }
+
+            plot_data[0]['showlegend'] = True
+
+        except Exception as err:
+            return HttpResponse(
+                json.dumps({
+                        'message': err.args[0],
+                        'source': 'raven.api',
+                        'class': 'FetchPlotDataAPIView',
+                    },
+                ),
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                content_type='application/json')
+
+        # plot_data = 42
         return Response(
             plot_data,
             status=status.HTTP_200_OK,

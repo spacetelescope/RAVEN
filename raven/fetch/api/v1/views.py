c10e5ad0fc48e8fb70a37e50418250caf7922f94
ddkauffman <27729134+ddkauffman@users.noreply.github.com>
2022-03-16 00:07:32 -0400

    fix request parameter

--- raven/fetch/api/v1/views.py
+++ raven/fetch/api/v1/views.py
@@ -53,7 +53,7 @@ class FetchFullResolutionData(APIView):
         try:
             msid = request.GET.get('msid')
             draw = request.GET.get('draw')
-            idx0 = int(request.GET.get('idx0'))
+            idx0 = int(request.GET.get('start'))
             length = int(request.GET.get('length'))
     
             tstart, tstop = self.default_date_range(request) 

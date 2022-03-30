96e5d28986f8a3e28bd83f616709d9aa95e91412
ddkauffman <27729134+ddkauffman@users.noreply.github.com>
2022-03-30 15:25:49 -0400

    Update apiv1_urls.py

--- raven/core/apiv1_urls.py
+++ raven/core/apiv1_urls.py
@@ -9,7 +9,7 @@ from raven.core.api.v1 import views as core_views
 api_version = {
     'MAJOR_VERSION': 2,
     'MINOR_VERSION': 4,
-    'PATCH_VERSION': 3
+    'PATCH_VERSION': 4
 }
 
 urlpatterns = [
